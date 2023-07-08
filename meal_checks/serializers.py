8c79c22bd2e01173b7b494481d1e726f6643da48
evgenijmartynuk07 <evgenijmartynuk07@gmail.com>
2023-07-08 17:54:39 +0300

    Add transaction

--- meal_checks/serializers.py
+++ meal_checks/serializers.py
@@ -4,4 +4,6 @@ from rest_framework import serializers
 class OrderSerializer(serializers.Serializer):
     order_id = serializers.IntegerField()
     order_details = serializers.CharField()
-    point_id = serializers.IntegerField()
+    point_id = serializers.IntegerField(
+        help_text="Where is printer located"
+    )
